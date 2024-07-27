085def3ad1150341ff02505b1f32d89bf733cd22
LDT <am.mouffok@gmail.com>
2024-07-27 03:33:49 +0200

    Initial Commit

---
+++ CallBlock/CallBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallBlockApp.swift
+//  CallBlock
+//
+//  Created by LADATA on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CallBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
